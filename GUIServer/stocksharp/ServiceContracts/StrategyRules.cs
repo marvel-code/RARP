9729a8626091b19c700508fe2e61747348dba9fd
IAmYosya <m9y.yosya@gmail.com>
2019-01-29 11:49:03 +0300

    Unificate strategy separating from code

--- GUIServer/stocksharp/ServiceContracts/StrategyRules.cs
+++ GUIServer/stocksharp/ServiceContracts/StrategyRules.cs
@@ -9,153 +9,139 @@ namespace stocksharp.ServiceContracts
 {
     public partial class WorkService
     {
-        private int ruleId;
-        private bool fix(bool _newBlock = false)
-        {
-            if (_newBlock)
-                //.
-                ruleId *= 10;
-            else
-            {
-                // Работаем в 10 системе счисления
-                if (ruleId % 10 == 9)
+        //
+        // Разрешение входа после выхода
+        //
+
+        private bool isEntryAllowedAfterExitRule =>
+                    //-// Разрешение входа после выхода
+
+                    true
+
+                    //-//
+                    && true;
+
+
+        //
+        // LONG
+        //
+
+        private bool LongCommonRule =>
+                    //-// LONG общее условие
+
+                    true
+
+                    //-//
+                    && true;
+
+        private List<List<bool>> LongAdditionalRules => new List<List<bool>>
                 {
-                    //addLogMessage("Незадача то какая.. Больше 9 условий в блоке!");
-                    //mw.stopTrading();
-                }
-                // ..
-                ruleId++;
-            }
+            //-// LONG добавочные условия
 
-            return true;
-        }
-
-        private const bool DYNAMIC = false; // true - включить; false - выключить (ДИНАМИЧЕСКОЕ УСЛОВИЕ)
-
-        private bool waitCommonReverse = false;
-        private bool waitLongReverse = false;
-        private bool waitShortReverse = false;
-        public TradeState updateTradeState(List<TimeFrame> tf, NeedAction _needAction, PartnerDataObject PD)
-        {
-            TradeState tradeState = new TradeState();
-
-            decimal Position_PNL = PD.Position_PNL;
-            decimal Position_PNL_MAX = PD.Position_PNL_MAX;
-
-            if (_needAction == NeedAction.LongOrShortOpen)
-            {
-
-
-                /** Условие на общий реверс **/
-
-                if (waitCommonReverse) waitCommonReverse = false;
-
-
-                /** Условие на LONG реверс **/
-
-                if (waitLongReverse) waitLongReverse = false;
-
-
-                /** Условие на SHORT реверс **/
-
-                if (waitShortReverse) waitShortReverse = false;
-                
-
-                /** LONG **/
-
-                {
-                    ruleId = 0;
-                    tradeState.LongOpen = !waitCommonReverse && !waitLongReverse
-                        //*//
-                        &&
+                    new List<bool>
+                    {
                         tf[0].adx[0].dip > tf[0].adx[0].dim
-                        //*//
-                        && true;
-                }
-                
+                    },
+                    
+        //-//
+                };
 
-                /** SHORT **/
 
-                if (!tradeState.LongOpen)
-                {
-                    ruleId = 0;
-                    tradeState.ShortOpen = !waitCommonReverse && !waitShortReverse
-                        //*//
-                        &&
+        //
+        // SHORT
+        //
+
+        private bool ShortCommonRule =>
+                        //-// SHORT общее условие
+
+                        true
+
+            //-//
+            && true;
+
+        private List<List<bool>> ShortAdditionalRules => new List<List<bool>>
+        {
+            //-// SHORT добавочные условия
+
+                    new List<bool>
+                    {
                         tf[0].adx[0].dip < tf[0].adx[0].dim
-                        //*//
-                        && true;
-                }
+                    },
+
+            //-//
+        };
 
 
-                /** Разрешение на LONG или SHORT **/
+        //
+        // Запреты на вход
+        //
 
-                bool allowEntry = true
-                    //*//
-                    && !tf[0].IsExitCandle()
-                    //*//
-                    && true;
+        private bool isEntryDenyed =>
+            //-// Запрет на вход 
 
+            tf[0].IsExitCandle()
 
-                /** Разрешение на LONG **/
+            //-//
+            && true;
 
-                tradeState.LongOpen &= allowEntry
-                    //*//
-                    //*//
-                    && true;
+        private bool isLongDenyed =>
+            //-// Запрет на LONG
 
+            false
 
-                /** Разрешение на SHORT **/
+            //-//
+            && true;
 
-                tradeState.ShortOpen &= allowEntry
-                    //*//
-                    //*//
-                    && true;
-                
+        private bool isShortDenyed =>
+            //-// Запрет на SHORT
 
-                tradeState.RuleId = ruleId;
-                if (tradeState.LongOpen) waitLongReverse = true;
-                if (tradeState.ShortOpen) waitShortReverse = true;
-            }
-            {
-                /** SELL **/
-                ruleId = 0;
-                tradeState.LongClose = false
-                    //*//
-                    //*//
-                    && true;
-                if (_needAction == NeedAction.LongClose)
-                    tradeState.RuleId = ruleId;
+            false
 
-                /** COVER **/
-                ruleId = 0;
-                tradeState.ShortClose = false
-                    //*//
-                    //*//
-                    && true;
+            //-//
+            && true;
 
 
-                if (_needAction == NeedAction.ShortClose)
-                    tradeState.RuleId = ruleId;
-            }
+        //
+        // SELL
+        //
 
-            /*
-            if (_currentUser == "ro#9019")
-            {
+        private bool SellCommonRule =>
+            //-// SELL общее условие
 
-                GUIServer.MainWindow.Instance.UpdateInfoLabel(string.Format("_{0}_ _{1}_ _{2}_ _{3}_ _{4}_ _{5}_ _{6}_",
-                    Math.Round(tf[0].Volume[0].GetAvrBv(120), 1),
-                    Math.Round(tf[0].Volume[0].GetAvrSv(120), 1),
-                    Math.Round(tf[0].Volume[0].GetAvrTv(120), 1),
-                    Math.Round(tf[0].Volume[0].GetAvrVv(120), 1),
-                   Math.Round(tf[0].Volume[0].GetTv(120), 1),
-                    Math.Round(tf[0].Volume[0].GetVv(120), 1),
-                   Math.Round(tf[0].Volume[0].sv, 1)
-                   ));
-            }
-            */
+            true
 
-            return tradeState;
-        }
+            //-//
+            && true;
+
+        private List<bool> SellAdditionalRules => new List<bool>
+        {
+            //-// SELL дополнительные условия
+            
+                tf[0].adx[0].dip < tf[0].adx[0].dim
+
+            //-//
+        };
+
+
+        //
+        // COVER
+        //
+
+        private bool CoverCommonRule =>
+            //-// COVER общее условие
+
+            true
+
+            //-//
+            && true;
+
+        private List<bool> CoverAdditionalRules => new List<bool>
+        {
+            //-// COVER дополнительные условия
+            
+                tf[0].adx[0].dip > tf[0].adx[0].dim
+
+            //-//
+        };
     }
 }
---
+++ GUIServer/stocksharp/ServiceContracts/UpdateTradeState.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using Ecng.Common;
+using StockSharp.BusinessEntities;
+using StockSharp.Messages;
+using transportDataParrern;
+
+namespace stocksharp.ServiceContracts
+{
+    public partial class WorkService
+    {
+        private const bool DYNAMIC = false; // true - включить; false - выключить (ДИНАМИЧЕСКОЕ УСЛОВИЕ)
+        private List<TimeFrame> tf;
+        private bool allow_entry = true;
+        private decimal Position_PNL;
+        private decimal Position_PNL_MAX;
+        private decimal Current_Price;
+
+        public TradeState updateTradeState(List<TimeFrame> _tf, NeedAction _needAction, PartnerDataObject PD)
+        {
+            int ruleId;
+            TradeState tradeState = new TradeState();
+            tradeState.RuleId = 0;
+            tradeState.LongOpen = false;
+            tradeState.LongClose = false;
+            tradeState.ShortOpen = false;
+            tradeState.ShortClose = false;
+            tf = _tf;
+            Position_PNL = PD.Position_PNL;
+            Position_PNL_MAX = PD.Position_PNL_MAX;
+            Current_Price = tf[0].GetCandle().ClosePrice;
+
+            if (_needAction == NeedAction.LongOrShortOpen)
+            {
+                if (!allow_entry && isEntryAllowedAfterExitRule)
+                {
+                    allow_entry = true;
+                }
+
+                if (allow_entry && !isEntryDenyed)
+                {
+                    // LONG
+                    {
+                        ruleId = 0;
+
+                        if (LongCommonRule && !isLongDenyed)
+                        {
+                            for (int i = 0; i < LongAdditionalRules.Count; i++)
+                            {
+                                ruleId *= 10;
+                                int index = LongAdditionalRules[i].FindIndex(x => x);
+                                if (index == -1)
+                                {
+                                    ruleId = 0;
+                                    break;
+                                }
+                                ruleId += index + 1;
+                            }
+
+                            if (ruleId != 0)
+                            {
+                                tradeState.LongOpen = true;
+                            }
+                        }
+                    }
+
+                    // SHORT
+                    if (!tradeState.LongOpen)
+                    {
+                        ruleId = 0;
+
+                        if (ShortCommonRule && !isShortDenyed)
+                        {
+                            for (int i = 0; i < ShortAdditionalRules.Count; i++)
+                            {
+                                ruleId *= 10;
+                                int index = ShortAdditionalRules[i].FindIndex(x => x);
+                                if (index == -1)
+                                {
+                                    ruleId = 0;
+                                    break;
+                                }
+                                ruleId += index + 1;
+                            }
+
+                            if (ruleId != 0)
+                            {
+                                tradeState.ShortOpen = true;
+                            }
+                        }
+                    }
+
+                    if (ruleId != 0)
+                    {
+                        tradeState.RuleId = ruleId;
+                        allow_entry = false;
+                    }
+                }
+            }
+            {
+                // Sell
+                ruleId = 0;
+                if (SellCommonRule)
+                {
+                    ruleId = SellAdditionalRules.FindIndex(x => x) + 1;
+                    if (ruleId != 0)
+                    {
+                        tradeState.LongClose = true;
+                        if (_needAction == NeedAction.LongClose)
+                        {
+                            tradeState.RuleId = ruleId;
+                        }
+                    }
+                }
+
+                // Cover
+                ruleId = 0;
+                if (CoverCommonRule)
+                {
+                    ruleId = CoverAdditionalRules.FindIndex(x => x) + 1;
+                    if (ruleId != 0)
+                    {
+                        tradeState.ShortClose = true;
+                        if (_needAction == NeedAction.ShortClose)
+                        {
+                            tradeState.RuleId = ruleId;
+                        }
+                    }
+                }
+            }
+
+            /*
+            if (_currentUser == "ro#9019")
+            {
+
+                GUIServer.MainWindow.Instance.UpdateInfoLabel(string.Format("_{0}_ _{1}_ _{2}_ _{3}_ _{4}_ _{5}_ _{6}_",
+                    Math.Round(tf[0].Volume[0].GetAvrBv(120), 1),
+                    Math.Round(tf[0].Volume[0].GetAvrSv(120), 1),
+                    Math.Round(tf[0].Volume[0].GetAvrTv(120), 1),
+                    Math.Round(tf[0].Volume[0].GetAvrVv(120), 1),
+                   Math.Round(tf[0].Volume[0].GetTv(120), 1),
+                    Math.Round(tf[0].Volume[0].GetVv(120), 1),
+                   Math.Round(tf[0].Volume[0].sv, 1)
+                   ));
+            }
+            */
+
+            return tradeState;
+        }
+    }
+}
